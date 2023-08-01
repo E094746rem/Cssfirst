c7610d50348c20e409c5cfd77c2a1ec9f58ce864
E094746rem <99578532+E094746rem@users.noreply.github.com>
2023-08-01 03:42:30 -0700

    Add files via upload
    CSS when first time i was learning

---
+++ try.js
@@ -0,0 +1,17 @@
+// console.log("Hello world");
+// console.log(typeof "efrem");
+// console.log(typeof 45);
+// console.log(typeof true);
+//String
+
+const myvariable = "mathimatic";
+
+//the length proprty
+// console.log("for example let me explian".length);
+//String Method
+//console.log(myvariable.charAt(3));retrun the char the position we provide
+//console.log(myvariable.indexOf("ti"));we provide the charater and it tell us we it is found by num
+//console.log(myvariable.lastIndexOf("th"));//it tels the last of it
+//console.log(myvariable.toLocaleUpperCase());return the value in all cap
+//console.log(myvariable.toLowerCase());return the value in lower case
+console.log(myvariable.includes("dis")); //this is a buleen the return if the value and the element didn't mach
